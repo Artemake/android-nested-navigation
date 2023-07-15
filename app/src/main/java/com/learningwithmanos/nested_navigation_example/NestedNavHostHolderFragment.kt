4fbefd2fe6e1b0de1623d28b755fcb31822ccdf6
E.Artemakis <emmanouil.artemakis@uxma.com>
2023-07-15 19:28:16 +0300

    added animations

--- app/src/main/java/com/learningwithmanos/nested_navigation_example/NestedNavHostHolderFragment.kt
+++ app/src/main/java/com/learningwithmanos/nested_navigation_example/NestedNavHostHolderFragment.kt
@@ -1,10 +1,14 @@
 package com.learningwithmanos.nested_navigation_example
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
 import com.learningwithmanos.nested_navigation_example.databinding.FragmentNestedNavHostHolderBinding
 
 class NestedNavHostHolderFragment: Fragment() {
@@ -18,6 +22,41 @@ class NestedNavHostHolderFragment: Fragment() {
     ): View? {
         binding = FragmentNestedNavHostHolderBinding.inflate(inflater, container, false)
 
+
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        binding.nestedNavHost.getFragment<NavHostFragment>().navController
+            .addOnDestinationChangedListener(
+                object : NavController.OnDestinationChangedListener {
+                    override fun onDestinationChanged(
+                        controller: NavController,
+                        destination: NavDestination,
+                        arguments: Bundle?
+                    ) {
+                        when(destination.id) {
+                            R.id.nestedFragment1 -> {
+                                binding.progressBar1.progress = 100
+                                binding.progressBar2.progress = 0
+                                binding.progressBar3.progress = 0
+                            }
+                            R.id.nestedFragment2 -> {
+                                binding.progressBar1.progress = 100
+                                binding.progressBar2.progress = 100
+                                binding.progressBar3.progress = 0
+                            }
+                            R.id.nestedFragment3 -> {
+                                binding.progressBar1.progress = 100
+                                binding.progressBar2.progress = 100
+                                binding.progressBar3.progress = 100
+                            }
+                        }
+                    }
+
+                }
+            )
     }
 }
